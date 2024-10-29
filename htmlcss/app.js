03b7127c9cb62b9f397dcbd041fe00f2d46add64
Hoai <hoaibkdn2012@gmail.com>
2024-10-28 22:59:51 -0700

    array method

--- htmlcss/app.js
+++ htmlcss/app.js
@@ -312,13 +312,248 @@ console.log('slicedArr2 ', slicedArr2)
  * 1. moveToTop2, you have to modify the current array
  * 2. sort the marks array following decrement 
  */
+// Hao
+function moveToTop3(arr) {
+  const index = arr.findIndex((a) => a?.name === "Yellow"); // 3 O(n)
+  let newArr = []
+  if (index !== -1) {
+    sliceItem = arr.splice(index, arr.length); // [3, 6) = O(n)
+    newArr = [...sliceItem, ...arr] // O(n)
+    return newArr
+    // return [...sliceItem, ...arr];
+    // console.log(sliceItem);
+  }
+  return arr;
+} // O(2n) ~ O(n)
+// console.log('moveToTop3 ', moveToTop3(marks));
+
+// Phuc
+function moveToTop4(arr, name) { // reference
+  console.log('arr ', arr)
+  const index = arr.findIndex(item => item?.name === name); // O(1)
+  console.log('index ', index)
+  if(index !== -1) {
+      const temporaryArray = arr.splice(index, 1); // [3, 1) O(1)
+      const removed = temporaryArray[0]; // {name: 'Yellow', mark: 6} O(1)
+
+      arr.unshift(removed);
+  }
+  return arr;
+} // => O(1) O(3n) ~ O(n)
+console.log('moveToTop4', moveToTop4(marks, 'Yellow'))
 
 
 
+// function decreaseSort(arr) { // 
+//   let result = [...arr].reduce((a, b) => {
+//     return a.mark > b.mark ? a : b;
+//   },);
+//   return result;
+// }
+
+// console.log(decreaseSort(marks));
+
+function sortDecrement(arr) {
+  const data = arr.filter(item => item)
+  data.sort((a, b) => a?.name > b?.name ? 1 : -1)
+  return data
+}
+
+console.log('data sorted', sortDecrement(marks))
+console.log('after sorting ', marks)
 
 
+// reduce
+const nums3 = [1, 3, 6, 0, 2, 9]
+
+function sum(nums) {
+  let sum = 0
+  for(let i = 0; i < nums.length; i++) {
+    if(!nums[i]) continue
+    sum += nums[i] // 4
+  }
+  return sum
+}
+console.log('sum ', sum(marks))
+
+function sum2(nums) {
+  return nums.reduce((prevValue, currValue) => prevValue + currValue, 0)
+}
+console.log('sum2 ', sum2(nums3))
+
+function sumMarks(marks) {
+  return marks.reduce((prevValue, currMark) => {
+    if(!currMark) {
+      return prevValue
+    }
+    return prevValue + currMark.mark // number
+  }, 0)
+}
+console.log('sumMarks ', sumMarks(marks))
+
+const products2 = [{
+  name: 'Iphone 16',
+  colors: ['white', 'ocean', 'pink', 'purple'], 
+  quantity: 20,
+  price: 20000000
+}, {
+  name: 'Iphone 16 promax',
+  colors: ['white', 'ocean', 'yellow'],
+  quantity: 40,
+  price: 30000000
+}, {
+  name: 'Iphone 15',
+  colors: ['purple'],
+  quantity: 13,
+  price: 10000000
+}, {
+  name: 'samsung zip 4',
+  colors: ['gray', 'green'],
+  quantity: 2,
+  price: 25000000
+}]
+
+// 1. return a new array: [{name: '', price: 20000000}, {name: '', price: 20000000}, {name: '', price: 20000000}]
+function convertProducts(products) {
+  return 
+}
 
 
+function convert(products) {
+  return products.reduce((init, currProduct) => {
+    if(currProduct) {
+      init.push({ name: currProduct.name, price: currProduct.price});
+    }
+    return init
+  }, []);
+}
+
+console.log(convert(products2));
+
+console.log(convertProducts(products2))
+
+function findIphone(products2) {
+  return products2.reduce((acc, product) => {
+      if (product?.name?.startsWith('Iphone')) {
+        acc.push({
+          name: product.name,
+          price: product.price
+        });
+      }
+      return acc;
+    }, []);
+}
+console.log(findIphone(products2));
+
+/**
+ * [{
+  name: 'Iphone 16',
+  colors: 'white, ocean, pink, purple', 
+}, {
+  name: 'Iphone 16 promax',
+  colors: 'white, ocean, yellow',
+}, {
+  name: 'Iphone 15',
+  colors: 'purple',
+  quantity: 13,
+  price: 10000000
+}, {
+  name: 'samsung zip 4',
+  colors: 'gray, green',
+}]
+ */
+
+// let sumColor = ''
+// for (let i = 0; i < arrColors.length; i++) {
+//   sumColor += `${arrColors[i]}${i === arrColors.length - 1 ? '' : ', '}`
+// }
+
+function convertColor(products2) {
+  return products2.reduce((acc, product) => {
+      if (product?.name?.startsWith('Iphone')) {
+          acc.push({
+              name: product.name,
+              colors: product.colors.join(', ')
+          });
+      }
+      return acc;
+  }, []);
+}
+
+// [1, 2, 4, 5] => '[1, 2, 4, 5]'
+function convert(products) {
+  return products.reduce((prevVal, currVal) => {
+    prevVal.push({
+      name: currVal.name,
+      colors: JSON.stringify(currVal.colors),
+    });
+    return prevVal;
+  }, []);
+}
+console.log('convertColor ', convertColor(products2))
+console.log('convert ', convert(products2));
  
 
+const keys = Object.keys(products[0])
+console.log('keys ', keys)
 
+const values = Object.values(products[0])
+console.log('values ', values)
+
+// const duplicatedNums = [4, 3, 1, 4, 4, 6, 2, 8, 2]
+
+// return array of unique numbers in any order  [4, 3, 1, 6, 2, 8] [3, 1, 4, 6, 2, 8]
+const duplicateNum = [4, 3, 1, 4, 4, 6 , 2, 8 ,2, 0];
+
+const findValue2 = duplicateNum.filter((item, index) => duplicateNum.indexOf(item) !== index);
+const findValue = duplicateNum.filter((item, index) => { // 0 O(n * n)
+  const indexDup = duplicateNum.indexOf(item)  // 0
+  if(indexDup !== index) // true
+    return item
+}); // [4, 4, 2]
+console.log('findValue ', findValue);
+
+function removeDuplicates2(arr) {
+  return Object.values(
+    arr.reduce((acc, curr)=> {
+      if(acc[curr] === undefined) {
+          acc[curr] = curr
+      }
+      return acc
+  }, {}) // O(n + n) => O(2n) => O(n)
+)
+}
+
+console.log('removeDuplicates2 ', removeDuplicates2(duplicateNum))
+
+const products3 = [{
+  name: 'Iphone 16',
+  colors: ['white', 'ocean', 'pink', 'purple'], 
+  quantity: 20,
+  price: 20000000
+}, {
+  name: 'Iphone 16 promax',
+  colors: ['white', 'ocean', 'yellow'],
+  quantity: 40,
+  price: 30000000
+}, {
+  name: 'Iphone 15',
+  colors: ['purple'],
+  quantity: 13,
+  price: 10000000
+}, {
+  name: 'samsung zip 4',
+  colors: ['gray', 'green'],
+  quantity: 2,
+  price: 14000000
+}]
+
+/**
+ * Homework:
+ * 1. write a convert function returning
+    { totalQuantity: 75, products: [{ name: '', price: '30,000,000' }] }
+   2. merge sorted numbers
+   given 2 array sorted numbers: nums1 = [0, 4, 9], nums2 = [2, 2, 4, 7, 9]
+   merge => return a new array = [0, 2, 2, 4, 4, 7, 9, 9]
+   // O(n + m)
+ */
