d4b04caaf4eb4e08710b444eb6bc8c63e3d8d86a
hoaitruong <hoaibkdn2012@gmail.com>
2024-11-11 08:09:03 -0800

    render list

--- react3/src/App.tsx
+++ react3/src/App.tsx
@@ -1,27 +1,75 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { Product } from './components';
 import './App.css';
+interface ProductModel {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const products: Array<ProductModel> = [
+  {
+    id: 1,
+    name: 'T-shirt',
+    price: 20000,
+  },
+  {
+    id: 2,
+    name: 'Sweater',
+    price: 50000,
+  },
+  {
+    id: 3,
+    name: 'Long Sleeve Shirt',
+    price: 120000,
+  },
+  {
+    id: 4,
+    name: 'Long Sleeve Shirt',
+    price: 520000,
+  },
+  {
+    id: 5,
+    name: 'Long Sleeve Shirt 2',
+    price: 110000,
+  },
+];
 
 function App() {
+  const [listProducts, setListProducts] = useState<Array<ProductModel>>(
+    products || []
+  );
+
+  const onClickItem = (id: number) => {
+    const itemIdx = listProducts.findIndex((item) => item?.id === id);
+    const newList = [...listProducts]; // clone
+    newList[itemIdx].price += 10000;
+    setListProducts(newList);
+  };
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit Hello hello 1111 <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      {listProducts.map((product: ProductModel) => {
+        return (
+          <Product
+            name={product.name}
+            price={product.price}
+            id={product.id}
+            onClick={onClickItem}
+          />
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+// state vs props
+
+/**
+ * Handle onClick
+ * when clicking on any item => update it into the top of the list
+ */
